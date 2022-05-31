a6d0374bea66c9aaa124b99b44e82c6f655918a2
Angel <santoyox714@gmail.com>
2022-05-31 00:57:08 -0700

    player combat updated

--- space-time-pilgrim/Assets/PlayerCombat.cs
+++ space-time-pilgrim/Assets/PlayerCombat.cs
@@ -4,43 +4,90 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    public Animator animator;
-    public Transform attackPoint;
-    public float attackRange= 0.5f;
-    public LayerMask enemyLayers;
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Mouse1))
-        {
-            Attack();
-        }
-        
-    }
-
-    void Attack()
-    {
-     animator.SetTrigger("Attack");
-
-     Collider2D [] hitEnemies= Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-     
-     foreach(Collider2D enemy in hitEnemies)
-     {
-      Debug.Log("hit " + enemy.name); 
-     }
-
- 
-    }
-
-
-    void DrawGizmosSelected()
-    {
-        if(attackPoint==null)
-        Debug.Log("attack point null");
-        return;
-
+    [SerializeField]
+    private bool combatEnabled;
+    [SerializeField]
+    private float inputTimer, attack1Radius, attack1Damage;
+    [SerializeField]
+    private Transform attack1HitBoxPos;
+    [SerializeField]
+    private LayerMask whatIsDamageable;
     
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-        
+    private bool gotInput, isAttacking, isFirstAttack;
+
+    private float lastInputTime = Mathf.NegativeInfinity;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("canAttack", combatEnabled);
     }
+
+    private void Update()
+    {
+        CheckCombatInput();
+        CheckAttacks();
+    }
+
+    private void CheckCombatInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (combatEnabled)
+            {
+                //Attempt combat
+                gotInput = true;
+                lastInputTime = Time.time;
+            }
+        }
+    }
+
+    private void CheckAttacks()
+    {
+        if (gotInput)
+        {
+            //Perform Attack1
+            if (!isAttacking)
+            {
+                gotInput = false;
+                isAttacking = true;
+                isFirstAttack = !isFirstAttack;
+                anim.SetBool("attack1", true);
+                anim.SetBool("firstAttack", isFirstAttack);
+                anim.SetBool("isAttacking", isAttacking);
+            }
+        }
+
+        if(Time.time >= lastInputTime + inputTimer)
+        {
+            //Wait for new input
+            gotInput = false;
+        }
+    }
+
+    private void CheckAttackHitBox()
+    {
+        Collider2D[] detectedObjects = Physics2D.OverlapCircleAll(attack1HitBoxPos.position, attack1Radius, whatIsDamageable);
+
+        foreach (Collider2D collider in detectedObjects)
+        {
+            collider.transform.parent.SendMessage("Damage", attack1Damage);
+            //Instantiate hit particle
+        }
+    }
+
+    private void FinishAttack1()
+    {
+        isAttacking = false;
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetBool("attack1", false);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attack1HitBoxPos.position, attack1Radius);
+    }
+
 }
